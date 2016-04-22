42af8dae02c336ef16681d8d42e95dc6c8a5d7f2
Book4212 <pasin_book@hotmail.com>
2016-04-22 16:20:03 +0700

    add data list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,23 +5,50 @@ angular.module('app.controllers', [])
 })
    
 .controller('tVCtrl', function($scope) {
-
+	$scope.tvs = [
+		{ title: "SAMSUNG 32 in. UA32FH4003" },
+		{ title: "SAMSUNG 40 in. UA40H5003" },
+		{ title: "SAMSUNG 24 in. UA24H4003" },
+		{ title: "SAMSUNG 55 in. UA55J6200AK" }
+	];
 })
    
 .controller('digitalTVCtrl', function($scope) {
-
+	$scope.tvs = [
+		{ title: "SAMSUNG 32 in. UA32J4003AK" },
+		{ title: "SAMSUNG 40 in. UA40J5100AK" },
+		{ title: "SAMSUNG 40 in. UA40J5125AK" },
+		{ title: "SAMSUNG 48 in. UA48J5100AK" },
+		{ title: "SAMSUNG 55 in. UA55JU6600K" }
+	];
 })
       
 .controller('vDOCtrl', function($scope) {
-
+	$scope.xtremer[
+		{ title: "Sony DVP-SR660P DVD Player"},
+		{ title: "Sony BDP-S1100 Blu Ray Player"},
+		{ title: "Sony BDP-S1200 Blu Ray Player"},
+		{ title: "Sony bdp-s5200 3d bluray player"},
+		{ title: "Sony BDP- S5500 Blu-Ray Player"}
+	];
 })
    
 .controller('audioCtrl', function($scope) {
-
+	$scope.audios = [
+		{ title: "SHAKE-X1D" },
+		{ title: "MHC-ECL7D" },
+		{ title: "GTK-N1BT"  }
+	];
 })
    
 .controller('xtremerCtrl', function($scope) {
-
+	$scope.xtremer[
+		{ title: "Xtreamer MK1"},
+		{ title: "Xtreamer Pro"},
+		{ title: "Xtreamer Ultra"},
+		{ title: "Xtreamer Prodigy Silver"},
+		{ title: "Xtreamer Prodigy 4K"}
+	];
 })
    
 .controller('menu2Ctrl', function($scope) {
@@ -29,23 +56,73 @@ angular.module('app.controllers', [])
 })
    
 .controller('addAudioCtrl', function($scope) {
-
+	$scope.audios = [
+		{ title: "SHAKE-X1D" },
+		{ title: "MHC-V7D" },
+		{ title: "MHC-GT3D" },
+		{ title: "SHAKE-3XD" },
+		{ title: "SHAKE-X7D" },
+		{ title: "MHC-M4V" },
+		{ title: "MHC-ECL7D" },
+		{ title: "GTK-N1BT"  }
+	];
 })
    
 .controller('addDigitalTVCtrl', function($scope) {
-
+	$scope.tvs = [
+		{ title: "SAMSUNG 24 in. UA24H4003" },
+		{ title: "SAMSUNG 32 in. UA32FH4003" },
+		{ title: "SAMSUNG 32 in. UA32J4003AK" },
+		{ title: "SAMSUNG 40 in. UA40H5003" },
+		{ title: "SAMSUNG 40 in. UA40J5100AK" },
+		{ title: "SAMSUNG 40 in. UA40J5125AK" },
+		{ title: "SAMSUNG 48 in. UA48J5100AK" },
+		{ title: "SAMSUNG 55 in. UA55J6200AK" },
+		{ title: "SAMSUNG 55 in. UA55JU6600K" }
+	];
 })
    
 .controller('addTVCtrl', function($scope) {
-
+	$scope.tvs = [
+		{ title: "SAMSUNG 24 in. UA24H4003" },
+		{ title: "SAMSUNG 32 in. UA32FH4003" },
+		{ title: "SAMSUNG 32 in. UA32J4003AK" },
+		{ title: "SAMSUNG 40 in. UA40H5003" },
+		{ title: "SAMSUNG 40 in. UA40J5100AK" },
+		{ title: "SAMSUNG 40 in. UA40J5125AK" },
+		{ title: "SAMSUNG 48 in. UA48J5100AK" },
+		{ title: "SAMSUNG 55 in. UA55J6200AK" },
+		{ title: "SAMSUNG 55 in. UA55JU6600K" }
+	];
 })
    
 .controller('addVDOCtrl', function($scope) {
-
+	$scope.xtremer[
+		{ title: "Sony DVP-SR370 DVD Player"},
+		{ title: "Sony DVP-SR660P DVD Player"},
+		{ title: "Sony BDP-S1500 Blu-Ray Video Player"},
+		{ title: "Sony DVP-SR760HP DVD Playe"},
+		{ title: "Sony BDP-S1100 Blu Ray Player"},
+		{ title: "Sony BDP-S1200 Blu Ray Player"},
+		{ title: "Sony BDP-S4100 Blu Ray Player"},
+		{ title: "Sony bdp-s5200 3d bluray player"},
+		{ title: "Sony BDPS3500 Blu-ray Player"},
+		{ title: "Sony BDP- S5500 Blu-Ray Player"}
+	];
 })
    
 .controller('addXtremerCtrl', function($scope) {
-
+	$scope.xtremer[
+		{ title: "Xtreamer MK1"},
+		{ title: "Xtreamer eTRAYz"},
+		{ title: "Xtreamer Pro"},
+		{ title: "iXtreamer"},
+		{ title: "Xtreamer Ultra"},
+		{ title: "Xtreamer Prodigy Silver"},
+		{ title: "Xtreamer Prodigy Black"},
+		{ title: "Xtreamer Ultra 2"},
+		{ title: "Xtreamer Prodigy 4K"}
+	];
 })
    
 .controller('audioControllerCtrl', function($scope) {
